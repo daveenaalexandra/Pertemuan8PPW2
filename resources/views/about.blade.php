40a19c98cea326810f09e0d36603fd2543682bf6
daveenaalexandra <daveenaalexandrapentury@mail.ugm.ac.id>
2025-10-29 22:59:37 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>Tentang Aplikasi Job Portal</h1>
+<p>Aplikasi ini akan membantu pengguna mencari dan melamar
+pekerjaan.</p>
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
